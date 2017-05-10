486a14a8e850057fbbba4a0a4fcd5b3632014d57
Mario <seelim@student.ethz.ch>
2017-05-10 21:32:41 +0200

    added basic functionality to ServoControlBoard

--- ServoControlBoard.py
+++ ServoControlBoard.py
@@ -1,6 +1,25 @@
+import Adafruit_PCA9685
 
 
 class ServoControlBoard(object):
 
+
     def __init__(self):
-        pass
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pwm.set_pwm_freq(60)
+        self.servo_min = 150  # Min pulse length out of 4096
+        self.servo_max = 600  # Max pulse length out of 4096
+
+    def set_servo_pulse(self, channel, pulse):
+        pulse_length = 1000000  # 1,000,000 us per second
+        pulse_length //= 60  # 60 Hz
+        print('{0}us per period'.format(pulse_length))
+        pulse_length //= 4096  # 12 bits of resolution
+        print('{0}us per bit'.format(pulse_length))
+        pulse *= 1000
+        pulse //= pulse_length
+        self.pwm.set_pwm(channel, 0, pulse)
+
+    def move(self, pos):
+        assert self.servo_min <= pos <= self.servo_max
+        self.pwm.set_pwm(0, 0, pos)
